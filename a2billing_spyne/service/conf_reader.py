18149a9b659253c0447e22dcdde1e99dd8c5a7e8
cemre <unalcmre@gmail.com>
2016-06-23 10:07:30 +0300

    SipBuddies table and conf_reader fixed and is now working

--- a2billing_spyne/service/conf_reader.py
+++ a2billing_spyne/service/conf_reader.py
@@ -44,19 +44,19 @@ for line in table:
         if name is not None and name != 'general' and data[0].isalpha():
             if data[0] in sip.keys():
                 if data[0] == "qualify" and len(data) >= 2:
-                    qualify = data[1]
+                    qualify = (data[1])[:-1]
                 elif data[0] == "type" and len(data) >= 2:
-                    type = data[1]
+                    type = (data[1])[:-1]
                 elif data[0] == "host" and len(data) >= 2:
-                    host = data[1]
+                    host = (data[1])[:-1]
                 elif data[0] == "context" and len(data) >= 2:
-                    context = data[1]
+                    context = (data[1])[:-1]
                 elif data[0] == "secret" and len(data) >= 2:
-                    secret = data[1]
+                    secret = (data[1])[:-1]
                 elif data[0] == "dtmfmode" and len(data) >= 2:
-                    dtmfmode = data[1]
+                    dtmfmode = (data[1])[:-1]
                 elif data[0] == "callerid" and len(data) >= 2:
-                    callerid = data[1]
-            print name,data
+                    callerid = (data[1])[:-1]
+            print name,data[1]
 
 session.commit()
